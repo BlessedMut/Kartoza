d4166410461567480bd75a0222079dbf454c1744
Blessed Mutengwa <blessedmutengwa@gmail.com>
2022-07-17 13:46:41 +0200

    -Added Profiles App to installed Apps -Added Home Front-End and updated static files path in settings.py -Updated views.py for the home view

--- profiles/views.py
+++ profiles/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Home Panel</h1>')
+    return render(request, 'profiles/home.html', {})
